a745754edb04686506b0a762507a3a3bbb756f4f
Dmitry Osipenko <digetx@gmail.com>
2015-07-01 18:42:06 +0300

    qemu-char.c: Pass CTRL+C to the guest
    Signed-off-by: Dmitry Osipenko <digetx@gmail.com>

--- qemu-char.c
+++ qemu-char.c
@@ -1139,6 +1139,7 @@ static void qemu_chr_set_echo_stdio(CharDriverState *chr, bool echo)
                           |INLCR|IGNCR|ICRNL|IXON);
         tty.c_oflag |= OPOST;
         tty.c_lflag &= ~(ECHO|ECHONL|ICANON|IEXTEN);
+        tty.c_lflag &= ~ISIG;
         tty.c_cflag &= ~(CSIZE|PARENB);
         tty.c_cflag |= CS8;
         tty.c_cc[VMIN] = 1;
